e134028f0f02204b483e0717488ae251c4f1d1fb
mcrwayfun <mcrwayfun@gmail.com>
2018-08-20 23:22:59 +0800

    添加分页插件pageHelper

--- src/main/java/com/qingtian/controller/VideoController.java
+++ src/main/java/com/qingtian/controller/VideoController.java
@@ -1,8 +1,15 @@
 package com.qingtian.controller;
 
+import com.github.pagehelper.PageHelper;
+import com.github.pagehelper.PageInfo;
+import com.qingtian.domain.Video;
 import com.qingtian.service.VideoService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 /**
  * @author mcrwayfun
@@ -30,8 +37,17 @@ public class VideoController {
     public Object pageVideo(@RequestParam(value = "page", defaultValue = "1") int page,
                             @RequestParam(value = "size", defaultValue = "10") int size) {
 
-        // 须要实现分页
-        return videoService.findAll();
+        PageHelper.startPage(page,size);
+
+        List<Video> list = videoService.findAll();
+        PageInfo<Video> pageInfo = new PageInfo<>(list);
+        Map<String,Object> data = new HashMap<>();
+        data.put("total_size",pageInfo.getTotal());//总条数
+        data.put("total_page",pageInfo.getPages());//总页数
+        data.put("current_page",page);//当前页
+        data.put("data",pageInfo.getList());//数据
+
+        return data;
     }
 
 
